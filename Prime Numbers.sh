c3da5d86b283c847e81bf2dfc8ffb232ee6f4638
Saiyan2208 <ashwinswain84@gmail.com>
2018-07-17 13:29:21 +0530

    Another Commit

--- Prime Numbers.sh
+++ Prime Numbers.sh
@@ -1,15 +1,15 @@
-for i in {2..100}
+for i in {2..100} #for i in numbers between 2 and 100
 do
 	flag=0
 	for (( j=2;j<=$((i/2));j++ ))
 	do
 		if [ $((i%j)) -eq 0 ]
 			then
-			flag=1
+			flag=1 #if number is not prime; flag=1
 			break
 		fi
 	done
-	if [[ $flag -eq 0 ]]; then
+	if [[ $flag -eq 0 ]]; then #print the prime number if flag=0
 		echo $i
 	fi
 done
